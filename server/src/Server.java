bc8e56dc64a625ba2553d4decdb807785765e7b2
nd5243 <nd5243@naver.com>
2017-06-03 00:00:19 +0900

    no message

--- server/src/Server.java
+++ server/src/Server.java
@@ -1,3 +1,5 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.sql.SQLException;
 
@@ -28,34 +30,49 @@ public class Server {
 			try {
 				myGraph = new Graph(GroceryDB.getRecipeDB(),GroceryDB.getIngredientDB());
 				int[] priority = new int[4];
-				priority[0] = 1;
-				priority[1] = 2;
-				priority[2] = 3;
-				priority[3] = 4;
+				priority[0] = 2;
+				priority[1] = 3;
+				priority[2] = 4;
+				priority[3] = 1;
 				int[] recentRecipes = new int [1];
-				recentRecipes[0] = 1;
+				recentRecipes[0] = 569; //레시피 변경 테스트
 				beforetime = System.currentTimeMillis();
 				double[][] result = myGraph.getUserRecomandRecipes("john419", priority, recentRecipes);
-				aftertime = System.currentTimeMillis();
+				aftertime = System.currentTimeMillis();	
 				Recipe data = null;
 				AppDB.Node Nodedata = null;
 				
 				for (int j = 0 ; j < Graph.getNodeList().size(); j++){
-					if (Graph.getNodeList().get(j).getRecipeId() == 1){
+					if (Graph.getNodeList().get(j).getRecipeId() == 569){	//레시피 변경 테스트
 						Nodedata = Graph.getNodeList().get(j);
 						break;
 					}
 				}
-				
+				BufferedWriter out = new BufferedWriter(new FileWriter("꿔바로우_2341.csv"));
+				String s = "순위,이름,총점,맛,재료,카테고리,나라\n";
+				out.write(s);
 				AppDB.Edge edgedata = null;
-				for (int i = 0 ; i < Graph.getNodeList().size(); i++){
+				for (int i = 0 ; i < Graph.getNodeList().size() - 1; i++){
 					data = GroceryDB.getRecipeDB().getRecipeInfoByID(Integer.toString((int)result[0][i]));
 					edgedata = Nodedata.getEdgeBytoRecipeId((int)result[0][i]);
-
-					System.out.println(i+"위 : "+data.getRecipeName() + " // " + result[1][i] + "           " + "맛 : " + edgedata.getValue(0)
+					try {
+					      ////////////////////////////////////////////////////////////////
+						  s = Integer.toString(i) + "," + data.getRecipeName() + ","+result[1][i] + "," + edgedata.getValue(0) + "," +
+								  edgedata.getValue(1) + "," + edgedata.getValue(2) + "," + edgedata.getValue(3) + "\n";
+					      out.write(s);
+					      
+					      ////////////////////////////////////////////////////////////////
+					    } catch (IOException e) {
+					        System.err.println(e); 
+					        
+					    }
+					/*System.out.println(i+"위 : "+data.getRecipeName() + " // " + result[1][i] + "           " + "맛 : " + edgedata.getValue(0)
 					+ "      재료 : " + edgedata.getValue(1));
-					
+					*/
 				}
+				out.close();
+			    System.out.println("파일 쓰기 완료");  
+				
 			} catch (SQLException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
